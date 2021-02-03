6247c3d649eedae3f5cf8c8fb8352ec8c3d340d1
dengxing <374294495@qq.com>
2021-02-03 16:58:33 +0800

    init

---
+++ mall-auth/src/main/java/com/dengxing/mallauth/MallAuthApplication.java
@@ -0,0 +1,13 @@
+package com.dengxing.mallauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAuthApplication.class, args);
+    }
+
+}
---
+++ mall-auth/src/test/java/com/dengxing/mallauth/MallAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dengxing.mallauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
