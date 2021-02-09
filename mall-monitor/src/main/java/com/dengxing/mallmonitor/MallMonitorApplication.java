7b262cd7e5eea25f1724a2f4d7e74bcf63195c19
dengxing <374294495@qq.com>
2021-02-09 16:15:59 +0800

    添加monitor 模块

---
+++ mall-monitor/src/main/java/com/dengxing/mallmonitor/MallMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.dengxing.mallmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallMonitorApplication.class, args);
+    }
+
+}
