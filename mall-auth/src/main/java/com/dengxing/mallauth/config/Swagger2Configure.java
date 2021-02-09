d870c7b1cb64cb2bd8deb94ced91ff91dec53e07
dengxing <374294495@qq.com>
2021-02-09 17:33:03 +0800

    knife4j

---
+++ mall-auth/src/main/java/com/dengxing/mallauth/config/Swagger2Configure.java
@@ -0,0 +1,22 @@
+package com.dengxing.mallauth.config;
+
+import com.dengxing.mallcommon.config.BaseSwaggerConfig;
+import com.dengxing.mallcommon.domain.SwaggerProperties;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Configure extends BaseSwaggerConfig {
+    @Override
+    public SwaggerProperties swaggerProperties() {
+        return SwaggerProperties.builder()
+                .apiBasePackage("com.dengxing.mallauth.controller")
+                .title("mall后台系统")
+                .description("mall后台相关接口文档")
+                .contactName("名字没想好\", \"http://baidu.com\", \"374294495@qq.com")
+                .version("1.0")
+                .enableSecurity(true)
+                .build();
+    }
+}
--- mall-gateway/src/main/java/com/dengxing/mallgateway/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/dengxing/mallgateway/MallGatewayApplication.java
@@ -2,8 +2,10 @@ package com.dengxing.mallgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MallGatewayApplication {
 
     public static void main(String[] args) {
