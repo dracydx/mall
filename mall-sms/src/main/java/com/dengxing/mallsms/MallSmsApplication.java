555ac6a0da3014ae7a1562ac113cb95ce6b91519
dengxing <374294495@qq.com>
2021-02-09 16:28:13 +0800

    其他模块的闯将

---
+++ mall-sms/src/main/java/com/dengxing/mallsms/MallSmsApplication.java
@@ -0,0 +1,13 @@
+package com.dengxing.mallsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallSmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallSmsApplication.class, args);
+    }
+
+}
